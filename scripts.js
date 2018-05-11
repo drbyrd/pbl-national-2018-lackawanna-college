d33ab4da30bd57a1f9861c674f7a05b2c354761a
Justin B <drbyrd@gmail.com>
2018-05-11 23:03:06 +0000

    Recoded stages, stages, updated basic html/css/jss structure, theme changes

--- scripts.js
+++ scripts.js
@@ -2,101 +2,84 @@
 // Stop Youtube video on close
 // Source: https://www.tutorialrepublic.com/faq/how-to-embed-youtube-video-inside-bootstrap-modal.php
 // ############################
-
-$(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var url = $("#cartoonVideo").attr('src');
-    
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $("#resumeModal").on('hide.bs.modal', function(){
-        $("#cartoonVideo").attr('src', '');
-    });
-    
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#resumeModal").on('show.bs.modal', function(){
-        $("#cartoonVideo").attr('src', url);
-    });
+$(document).ready(function() {
+	/* Get iframe src attribute value i.e. YouTube video url
+	and store it in a variable */
+	var url = $("#cartoonVideo").attr('src');
+	/* Assign empty url value to the iframe src attribute when
+	modal hide, which stop the video playing */
+	$("#resumeModal").on('hide.bs.modal', function() {
+		$("#cartoonVideo").attr('src', '');
+	});
+	/* Assign the initially stored url back to the iframe src
+	attribute when modal is displayed again */
+	$("#resumeModal").on('show.bs.modal', function() {
+		$("#cartoonVideo").attr('src', url);
+	});
 });
-
-$(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var url = $("#cartoonVideo2").attr('src');
-    
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $("#applicationModal").on('hide.bs.modal', function(){
-        $("#cartoonVideo2").attr('src', '');
-    });
-    
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#applicationModal").on('show.bs.modal', function(){
-        $("#cartoonVideo2").attr('src', url);
-    });
+$(document).ready(function() {
+	/* Get iframe src attribute value i.e. YouTube video url
+	and store it in a variable */
+	var url = $("#cartoonVideo2").attr('src');
+	/* Assign empty url value to the iframe src attribute when
+	modal hide, which stop the video playing */
+	$("#applicationModal").on('hide.bs.modal', function() {
+		$("#cartoonVideo2").attr('src', '');
+	});
+	/* Assign the initially stored url back to the iframe src
+	attribute when modal is displayed again */
+	$("#applicationModal").on('show.bs.modal', function() {
+		$("#cartoonVideo2").attr('src', url);
+	});
 });
-
-$(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var url = $("#cartoonVideo3").attr('src');
-    
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $("#interviewModal").on('hide.bs.modal', function(){
-        $("#cartoonVideo3").attr('src', '');
-    });
-    
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#interviewModal").on('show.bs.modal', function(){
-        $("#cartoonVideo3").attr('src', url);
-    });
+$(document).ready(function() {
+	/* Get iframe src attribute value i.e. YouTube video url
+	and store it in a variable */
+	var url = $("#cartoonVideo3").attr('src');
+	/* Assign empty url value to the iframe src attribute when
+	modal hide, which stop the video playing */
+	$("#interviewModal").on('hide.bs.modal', function() {
+		$("#cartoonVideo3").attr('src', '');
+	});
+	/* Assign the initially stored url back to the iframe src
+	attribute when modal is displayed again */
+	$("#interviewModal").on('show.bs.modal', function() {
+		$("#cartoonVideo3").attr('src', url);
+	});
 });
-
 // ############################
 // Scroll to anchor 
 // Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/
 // ############################
-
 $(function() {
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+	// Select all links with hashes
+	$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]').not('[href="#0"]').click(function(event) {
+			// On-page links
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				// Figure out element to scroll to
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				// Does a scroll target exist?
+				if (target.length) {
+					// Only prevent default if animation is actually gonna happen
+					event.preventDefault();
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000, function() {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) { // Checking if the target was focused
+							return false;
+						} else {
+							$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						};
+					});
+				}
+			}
+		});
 });
